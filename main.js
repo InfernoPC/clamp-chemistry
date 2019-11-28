ac38269a8b14b80a61f7610eb6e2a6fc40e356c8
Tim Chen <inferno6562@gmail.com>
2019-11-28 11:08:15 +0800

    split html, js, and css

---
+++ main.js
@@ -0,0 +1,45 @@
+$( document ).ready(function() {
+	// table 1
+  $( "button#getWeight" ).on( "click", function( event ) {
+  	var mole_weight = $("#mole_weight").val();
+  	var volumn = $("#volumn").val() * parseFloat($('input[name=volumn_unit]:checked', '#table1').val());
+  	var density = $("#density").val() * parseFloat($('input[name=density_unit]:checked', '#table1').val());
+  	var unit_conversion = parseFloat($('input[name=weight_unit]:checked', '#table1').val());
+  	$("#weight").val((mole_weight*volumn*density/unit_conversion).toFixed(4));
+	});
+  $( "button#getVolumn" ).on( "click", function( event ) {
+  	var mole_weight = $("#mole_weight").val();
+  	var weight = $("#weight").val() * parseFloat($('input[name=weight_unit]:checked', '#table1').val());
+  	var density = $("#density").val() * parseFloat($('input[name=density_unit]:checked', '#table1').val());
+  	var unit_conversion = parseFloat($('input[name=volumn_unit]:checked', '#table1').val());
+		$("#volumn").val(((weight/mole_weight/density)/unit_conversion).toFixed(4));
+	});
+  $( "button#getDensity" ).on( "click", function( event ) {
+  	var mole_weight = $("#mole_weight").val();
+  	var weight = $("#weight").val() * parseFloat($('input[name=weight_unit]:checked', '#table1').val());
+  	var volumn = $("#volumn").val() * parseFloat($('input[name=volumn_unit]:checked', '#table1').val());
+  	var unit_conversion = parseFloat($('input[name=density_unit]:checked', '#table1').val());
+  	$("#density").val(((weight/mole_weight/volumn)/unit_conversion).toFixed(4));
+	});
+
+  // table 2
+  $( "button#getDilution" ).on( "click", function( event ) {
+    var ori_density = $("#ori_density").val() * parseFloat($('input[name=ori_density_unit]:checked', '#table2').val());
+    var dilution_density = $("#dilution_density").val() * parseFloat($('input[name=dilution_density_unit]:checked', '#table2').val());
+    var dilution_volumn = $("#dilution_volumn").val();
+    var use_ori_density_conversion = parseFloat($('input[name=dilution_volumn_unit]:checked', '#table2').val()) / parseFloat($('input[name=use_ori_density_unit]:checked', '#table2').val());
+    var diluent_conversion = parseFloat($('input[name=dilution_volumn_unit]:checked', '#table2').val()) / parseFloat($('input[name=diluent_unit]:checked', '#table2').val());
+    $("#use_ori_density").val((dilution_volumn*dilution_density/ori_density)*use_ori_density_conversion);
+    var dilution_ratio = ori_density / dilution_density;
+    if(dilution_ratio >= 1000){
+  	  // ignore stock volumn
+      $("#diluent").val((dilution_volumn*diluent_conversion).toFixed(4));
+    }else{
+      $("#diluent").val(((dilution_volumn*(1-dilution_density/ori_density))*diluent_conversion).toFixed(4));
+    }
+  });
+
+  // tabs
+  $( "#tabs" ).tabs();
+});
+
